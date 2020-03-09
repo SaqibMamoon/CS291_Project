f0747ed561ead8ea39229ba5675f8025f5b4a67f
moslehm <32352647+moslehm@users.noreply.github.com>
2020-03-09 09:54:35 -0600

    Add tabControl with Employee, Customer, and Charts tabs.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,73 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+            this.Reports = new System.Windows.Forms.TabControl();
+            this.tabEmployee = new System.Windows.Forms.TabPage();
+            this.tabCustomer = new System.Windows.Forms.TabPage();
+            this.tabCharts = new System.Windows.Forms.TabPage();
+            this.Reports.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Reports
+            // 
+            this.Reports.Controls.Add(this.tabEmployee);
+            this.Reports.Controls.Add(this.tabCustomer);
+            this.Reports.Controls.Add(this.tabCharts);
+            this.Reports.Location = new System.Drawing.Point(13, 13);
+            this.Reports.Name = "Reports";
+            this.Reports.SelectedIndex = 0;
+            this.Reports.Size = new System.Drawing.Size(775, 425);
+            this.Reports.TabIndex = 0;
+            // 
+            // tabEmployee
+            // 
+            this.tabEmployee.Location = new System.Drawing.Point(4, 22);
+            this.tabEmployee.Name = "tabEmployee";
+            this.tabEmployee.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEmployee.Size = new System.Drawing.Size(767, 399);
+            this.tabEmployee.TabIndex = 0;
+            this.tabEmployee.Text = "Employee";
+            this.tabEmployee.UseVisualStyleBackColor = true;
+            // 
+            // tabCustomer
+            // 
+            this.tabCustomer.Location = new System.Drawing.Point(4, 22);
+            this.tabCustomer.Name = "tabCustomer";
+            this.tabCustomer.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCustomer.Size = new System.Drawing.Size(767, 399);
+            this.tabCustomer.TabIndex = 1;
+            this.tabCustomer.Text = "Customer";
+            this.tabCustomer.UseVisualStyleBackColor = true;
+            // 
+            // tabCharts
+            // 
+            this.tabCharts.Location = new System.Drawing.Point(4, 22);
+            this.tabCharts.Name = "tabCharts";
+            this.tabCharts.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCharts.Size = new System.Drawing.Size(767, 399);
+            this.tabCharts.TabIndex = 2;
+            this.tabCharts.Text = "Charts";
+            this.tabCharts.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Reports);
+            this.Name = "Form1";
+            this.Text = "MEK";
+            this.Reports.ResumeLayout(false);
+            this.ResumeLayout(false);
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.TabControl Reports;
+        private System.Windows.Forms.TabPage tabEmployee;
+        private System.Windows.Forms.TabPage tabCustomer;
+        private System.Windows.Forms.TabPage tabCharts;
+    }
 }
 
