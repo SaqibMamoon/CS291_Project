dffdd5ab7066162689901fa158a49269f0089058
Xavier Kidston <kidstonx@gmail.com>
2020-03-20 18:14:17 -0600

    Added some charts, going to look into sliding toolbars and calendars with generating graphs.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,27 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.Reports = new System.Windows.Forms.TabControl();
             this.tabEmployee = new System.Windows.Forms.TabPage();
             this.tabCustomer = new System.Windows.Forms.TabPage();
             this.tabCharts = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.Reports.SuspendLayout();
+            this.tabCharts.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             this.SuspendLayout();
             // 
             // Reports
@@ -40,51 +56,112 @@
             this.Reports.Controls.Add(this.tabEmployee);
             this.Reports.Controls.Add(this.tabCustomer);
             this.Reports.Controls.Add(this.tabCharts);
-            this.Reports.Location = new System.Drawing.Point(13, 13);
+            this.Reports.Location = new System.Drawing.Point(17, 16);
+            this.Reports.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Reports.Name = "Reports";
             this.Reports.SelectedIndex = 0;
-            this.Reports.Size = new System.Drawing.Size(775, 425);
+            this.Reports.Size = new System.Drawing.Size(1033, 523);
             this.Reports.TabIndex = 0;
             // 
             // tabEmployee
             // 
-            this.tabEmployee.Location = new System.Drawing.Point(4, 22);
+            this.tabEmployee.Location = new System.Drawing.Point(4, 25);
+            this.tabEmployee.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabEmployee.Name = "tabEmployee";
-            this.tabEmployee.Padding = new System.Windows.Forms.Padding(3);
-            this.tabEmployee.Size = new System.Drawing.Size(767, 399);
+            this.tabEmployee.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabEmployee.Size = new System.Drawing.Size(1025, 494);
             this.tabEmployee.TabIndex = 0;
             this.tabEmployee.Text = "Employee";
             this.tabEmployee.UseVisualStyleBackColor = true;
             // 
             // tabCustomer
             // 
-            this.tabCustomer.Location = new System.Drawing.Point(4, 22);
+            this.tabCustomer.Location = new System.Drawing.Point(4, 25);
+            this.tabCustomer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabCustomer.Name = "tabCustomer";
-            this.tabCustomer.Padding = new System.Windows.Forms.Padding(3);
-            this.tabCustomer.Size = new System.Drawing.Size(767, 399);
+            this.tabCustomer.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabCustomer.Size = new System.Drawing.Size(1025, 494);
             this.tabCustomer.TabIndex = 1;
             this.tabCustomer.Text = "Customer";
             this.tabCustomer.UseVisualStyleBackColor = true;
             // 
             // tabCharts
             // 
-            this.tabCharts.Location = new System.Drawing.Point(4, 22);
+            this.tabCharts.Controls.Add(this.chart3);
+            this.tabCharts.Controls.Add(this.chart2);
+            this.tabCharts.Controls.Add(this.chart1);
+            this.tabCharts.Location = new System.Drawing.Point(4, 25);
+            this.tabCharts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabCharts.Name = "tabCharts";
-            this.tabCharts.Padding = new System.Windows.Forms.Padding(3);
-            this.tabCharts.Size = new System.Drawing.Size(767, 399);
+            this.tabCharts.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabCharts.Size = new System.Drawing.Size(1025, 494);
             this.tabCharts.TabIndex = 2;
             this.tabCharts.Text = "Charts";
             this.tabCharts.UseVisualStyleBackColor = true;
+            this.tabCharts.Click += new System.EventHandler(this.tabCharts_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(22, 20);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(300, 226);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(305, 20);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(276, 226);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart3.Legends.Add(legend1);
+            this.chart3.Location = new System.Drawing.Point(560, 20);
+            this.chart3.Name = "chart3";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart3.Series.Add(series1);
+            this.chart3.Size = new System.Drawing.Size(269, 226);
+            this.chart3.TabIndex = 2;
+            this.chart3.Text = "chart3";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 543);
             this.Controls.Add(this.Reports);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "MEK";
             this.Reports.ResumeLayout(false);
+            this.tabCharts.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             this.ResumeLayout(false);
 
 		}
@@ -95,6 +172,9 @@
         private System.Windows.Forms.TabPage tabEmployee;
         private System.Windows.Forms.TabPage tabCustomer;
         private System.Windows.Forms.TabPage tabCharts;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace CS291_Project
 		{
 			InitializeComponent();
 		}
+
+		private void tabCharts_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
