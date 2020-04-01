d9811dade21f2bc8fdf9430a18948fbdfa2ff421
Xavier Kidston <kidstonx@gmail.com>
2020-03-31 19:26:07 -0600

    Updated so a branch is created on startup so we can add employees.  All Id's start at 1. To Add: -Error handling if the chosen employee wants to pick a branch that doesn't exist

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CS291_Project
@@ -13,7 +15,29 @@ namespace CS291_Project
 		/// </summary>
 		[STAThread]
 		static void Main()
-	{
+		{
+
+			using (SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;" +
+																	"AttachDbFilename=|DataDirectory|Database1.mdf;" +
+																	"Integrated Security=True"))
+			{
+				conn.Open();
+				SqlCommand comm = new SqlCommand();
+				comm.Connection = conn;
+				comm.CommandText = "select count (*) from branch";
+				int userCount = Convert.ToInt32(comm.ExecuteScalar());
+
+				if (userCount == 0)
+				{
+					comm.CommandText = "insert into branch (name) values (@name)";
+					comm.Parameters.AddWithValue("@name", "Xaviers Branch");
+					comm.ExecuteNonQuery();
+				}
+
+				conn.Close();
+
+			}
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new LoginForm());
